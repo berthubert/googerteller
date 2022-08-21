6dc02ef81f64f592147f3fdd2e74340e1aa88dff
bert hubert <bert@hubertnet.nl>
2022-08-21 22:48:40 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # googerteller
 audible feedback on just how much your browsing feeds into google
+
+## How to compile
+First install libpcaudio, then:
+
+```
+cmake .
+make
+```
+
+## How to run:
+
+```
+sudo tcpdump -n -l dst net 1.2.3.4/32 $(for a in $(jq .prefixes < goog.json  | grep ipv4Prefix | cut -f2 -d: | sed s/\"//g); do echo or dst net $a; done)  |  ./teller 
+```
+
+And then cry.
---
+++ teller.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <unistd.h>
+#include <thread>
+#include <atomic>
+#include <pcaudiolib/audio.h>
+
+using namespace std;
+
+int main()
+{
+  audio_object* ao;
+
+  ao=create_audio_device_object(0, "teller", "");
+  if(!ao) {
+    cerr<<"Unable to open audio file "<<endl;
+    return 0;
+  }
+  
+  int res = audio_object_open(ao, AUDIO_OBJECT_FORMAT_S16LE, 44100, 1);
+  if(res < 0) {
+    cerr<<"Error opening audio: "<<audio_object_strerror(ao, res)<<endl;
+  }
+
+  std::atomic<int64_t> counter;
+  auto player = [&]() {
+    vector<int16_t> data;
+    int ourcounter=0;
+    data.reserve(44100);
+    while(counter >= 0) {
+      data.clear();
+      if(ourcounter < counter) {
+        for(int n=0; n < 250; ++n) {
+          int16_t val = 20000 * sin((n/44100.0) * 500 * 2 * M_PI);
+          data.push_back(val);
+        }
+        ourcounter++;
+      }
+      else {
+        for(int n=0; n < 150; ++n) {
+          data.push_back(0);
+        }
+      }
+      
+      audio_object_write(ao, &data[0], data.size() * sizeof(decltype(data)::value_type));
+      // audio_object_flush(ao);
+    }
+  };
+
+  std::thread athread(player);
+  string line;
+  while(getline(cin, line)) {
+    counter++;
+  }
+  counter = -1;
+  athread.join();
+  sleep(1);
+}
