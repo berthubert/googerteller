e7897a9832930eddc616d199cd2f515ca487bc00
bert hubert <bert@hubertnet.nl>
2022-08-29 16:17:42 +0200

    abstract out lpmwrapper, add test

--- teller.cc
+++ teller.cc
@@ -6,46 +6,10 @@
 #include <atomic>
 #include <pcaudiolib/audio.h>
 #include "ext/toml.hpp"
-extern "C" {
-#include "ext/lpm.h"
-}
+#include "lpmwrapper.hh"
 
 using namespace std;
 
-class LPMWrapper
-{
-public:
-  LPMWrapper()
-  {
-    d_lpm = lpm_create();
-  }
-  ~LPMWrapper()
-  {
-    lpm_destroy(d_lpm);
-  }
-
-  void insert(const std::string& str, void* val=(void*)1)
-  {
-    char addr[16];
-    size_t len=sizeof(addr);
-    unsigned preflen=0;
-    lpm_strtobin(str.c_str(), addr, &len, &preflen);
-    if(lpm_insert(d_lpm, addr, len, preflen, val) < 0)
-      throw std::runtime_error("Error inserting prefix");    
-  }
-
-  void* lookup(const char*str)
-  {
-    char addr[16];
-    size_t len=sizeof(addr);
-    unsigned preflen=0;
-    lpm_strtobin(str, addr, &len, &preflen);
-    return lpm_lookup(d_lpm, addr, len);
-  }
-  
-private:
-  lpm_t* d_lpm;
-};
 
 struct TrackerConf
 {
--- testrunner.cc
+++ testrunner.cc
@@ -1,10 +1,15 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "ext/doctest.h"
-#include "ext/lpm.h"
+#include "lpmwrapper.hh"
 using namespace std;
 
 TEST_CASE("basic test") {
-  CHECK(1==1);
+  LPMWrapper t;
+  void* ptr = (void*)1;
+  t.insert("127.0.0.1/32", ptr);
+  
+  CHECK(t.lookup("127.0.0.1") == ptr);
+  CHECK(t.lookup("127.0.0.2") == 0);
 }
 
 
